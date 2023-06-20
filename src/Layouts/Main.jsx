206fa17fe335317c5891523c8d11323cfdb938f1
f-rahim <freelancerrahim26@gmail.com>
2023-06-20 12:13:47 +0600

    Scroll top when url/location was changed

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,8 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Shared/Header/Header";
 import Footer from "../components/Shared/Footer/Footer";
+import { useEffect } from "react";
 
 const Main = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [location]);
+
   return (
     <>
       <Header></Header>
